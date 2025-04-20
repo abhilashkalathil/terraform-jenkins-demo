27ac1e414514cc90dbfadb219604c14998d111ae
abhilashkalathil <abi.babu@gmail.com>
2025-04-20 13:17:33 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "172.31.0.0/16"
-  availability_zone = "ap-south-1"
+  availability_zone = "ap-south-1a"
 
   tags = {
     Name = "public-subnet"
